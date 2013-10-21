04de56be3af3c1dd0fa1ee808196ada76ac41ce8
George Caley <george@caley.com.au>
2013-10-22 10:45:23 +1100

    22/10 0900-1045: beefed up the registration form and added some basic client-side validation

--- js/mekong.js
+++ js/mekong.js
@@ -7,3 +7,33 @@ function openRegisterModal() {
     $("#signInModal").modal("hide");
     $("#registerModal").modal();
 }
+
+function register() {
+    // do client side validation of the inputs
+
+    var problem = "";
+
+    var username = $("#registerForm input[name=username]").val();
+    if (username == "") {
+        problem = "Please enter a username.";
+    } else {
+
+        var password = $("#registerForm input[name=password]").val();
+        var password2 = $("#registerForm input[name=password2]").val();
+        if (password == "") {
+            problem = "Please enter a password.";
+        } else if (password != password2) {
+            problem = "Make sure both passwords you entered match.";
+        }
+
+    }
+
+    if (problem) {
+        $("#registerAlert").text(problem);
+        $("#registerAlert").fadeIn();
+    } else {
+        $("#registerAlert").fadeOut();
+    }
+
+    return false;
+}
